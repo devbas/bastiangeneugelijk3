60fcf264a8c92b51c138eb649577aced3927fd09
Bastian Geneugelijk <bastiangeneugelijk@me.com>
2020-03-07 23:58:23 +0100

    Netlify build fix

---
+++ static/js/tex-svg-config.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+  window.MathJax = {
+    tex: {
+      inlineMath: [['$', '$'], ['\\(', '\\)']]
+    },
+    svg: {
+      fontCache: 'global'
+    }
+  };
+})
